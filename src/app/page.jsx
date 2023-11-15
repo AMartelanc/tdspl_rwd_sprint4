08d0a4a4f4ea4db97ab058b287757dc0e66a6116
guibk <gui.batista0709@gmail.com>
2023-11-15 19:44:17 -0300

    Textos na página principal e alunos no rodapé

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './globals.css'
 
 export default function Home() {
   return (
-    <h1> Home page </h1>
-
+    <main>
+      <div>
+        <div>
+        <h1 className=""> Sobre nós </h1>
+          <p>O nosso projeto tem como objetivo principal a implementação de um sistema automatizado de vistoria de bicicletas, utilizando APIs de reconhecimento de objetos. Nossa proposta visa tornar o processo de vistoria mais eficiente, ágil e seguro, eliminando a necessidade do cliente fazer o upload de imagens da bicicleta.</p>
+          <p>Para alcançar esse objetivo, iremos integrar APIs de reconhecimento de objetos em diferentes etapas do processo de vistoria. Inicialmente, o cliente será direcionado para uma tela de validação, onde será solicitado que ele confirme sua presença e inicie o processo de vistoria.</p>
+          <p>A partir desse momento, a IA (Inteligência Artificial) entrará em ação, analisando as imagens em tempo real, capturadas pela câmera do dispositivo do cliente. Através do uso de algoritmos avançados de reconhecimento de objetos, a IA será capaz de identificar a bicicleta e suas características específicas, como marca, modelo, cor, tamanho, entre outras informações relevantes.</p>
+          <p>Além disso, a IA também será treinada para verificar se há algum dano visível na bicicleta que possa inviabilizar a contratação do seguro. Caso a IA detecte algum problema, será gerado um alerta para o cliente, informando a necessidade de reparo antes de prosseguir com o processo de seguro.</p>
+        </div>
+      </div>
+    </main>
   )
 }
--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,7 +4,8 @@ export default function Rodape() {
   return (
     <footer className="bg-zinc-800 flex justify-center items-center h-15 w-full h-15">
         <div className="flex flex-col justify-center p-8 text-white font-bold">
-          <p>Alunos: <span>Alexandre Martelanc - rm550247 | João Vitor Souza Nunes - rm550381 | Denise Nascimento Baptistucci - rm550511 | Matheus Colossal Araujo - rm99572 | Guilherme Costa Batista - rm551173</span></p>
+          <p>Alunos: Alexandre Martelanc - rm550247 | João Vitor Souza Nunes - rm550381 | Denise Nascimento Baptistucci - rm550511
+             | Matheus Colossal Araujo - rm99572 | Guilherme Costa Batista - rm551173</p>
         </div>
     </footer>
   )
